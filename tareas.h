7d8b968063aea95b1cb59a8d4a361cb68de9a28d
Saider <ferreirasaid55@gmail.com>
2023-12-29 17:49:07 -0600

    Función mostrarTareasPorFecha añadida

--- tareas.h
+++ tareas.h
@@ -6,7 +6,7 @@ void agregarTarea(const char *nombreArchivo);
 void mostrarTareas(const char *nombreArchivo);
 void marcarComoCompletada(const char *nombreArchivo, const char *descripcion);
 void eliminarTarea(const char *nombreArchivo, const char *descripcion);
-
+void mostrarTareasPorFecha(const char *nombreArchivo);
 
 // funciones para validar datos
 int validarFormatoFecha(const char *fecha);
