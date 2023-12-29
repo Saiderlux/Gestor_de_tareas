0866c6b22309967594c90e531dba827cf4683e2f
Saider <ferreirasaid55@gmail.com>
2023-12-29 17:50:08 -0600

    añadí un menu de opciones para consultar las tareas pendientes

--- gestor_de_tareas.c
+++ gestor_de_tareas.c
@@ -4,14 +4,56 @@
 #include <stdlib.h>
 #include <string.h>
 
-
+int menuMostrar();
 
 int main()
 {
     const char *nombreArchivo = "tareas.txt";
 
     agregarTarea(nombreArchivo);
-    mostrarTareas(nombreArchivo);
+    menuMostrar();
+
+    return 0;
+}
+
+int menuMostrar()
+{
+    const char *nombreArchivo = "tareas.txt";
+    int opcion;
+    char fecha[20];
+    char prioridad[20];
+
+    do
+    {
+        // Menú de opciones
+        printf("Menu de Opciones:\n");
+        printf("1. Mostrar todas las tareas\n");
+        printf("2. Mostrar tareas por fecha\n");
+        printf("3. Mostrar tareas por prioridad\n");
+        printf("0. Salir\n");
+        printf("Ingrese su opcion: ");
+        scanf("%d", &opcion);
+
+        switch (opcion)
+        {
+        case 1:
+            mostrarTareas(nombreArchivo);
+            break;
+        case 2:
+
+            mostrarTareasPorFecha(nombreArchivo);
+            break;
+        case 3:
+           
+        case 0:
+            printf("Saliendo del programa. ¡Hasta luego!\n");
+            break;
+        default:
+            printf("Opción no válida. Inténtelo de nuevo.\n");
+            break;
+        }
+
+    } while (opcion != 0);
 
     return 0;
 }
