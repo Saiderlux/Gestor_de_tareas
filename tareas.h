cf6efb5cb147e340fbce8789fa2ae759f45ca723
Saider <ferreirasaid55@gmail.com>
2023-12-29 18:28:57 -0600

    Funcion mostrarTareasPorPrioridad añadida

--- tareas.h
+++ tareas.h
@@ -7,6 +7,7 @@ void mostrarTareas(const char *nombreArchivo);
 void marcarComoCompletada(const char *nombreArchivo, const char *descripcion);
 void eliminarTarea(const char *nombreArchivo, const char *descripcion);
 void mostrarTareasPorFecha(const char *nombreArchivo);
+void mostrarTareasPorPrioridad(const char *nombreArchivo);
 
 // funciones para validar datos
 int validarFormatoFecha(const char *fecha);
